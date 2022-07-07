d385932e89756ce294598bb2b066d5a45ba6be7e
emottley <104531780+emottley@users.noreply.github.com>
2022-07-07 17:34:13 -0400

    Creation of basic html file. Creation of basic  js file.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+window.alert("Hello, World!"); //Using alert function to open alert window at start of webpage
+document.write("document write"); //Writing text to file
+
+var A = " This is a string"; //Declaring a function A
+document.write(A); //Writing a variable to a file
+
+document.write(" He said \"Hello World\""); //Writing to a file and implictly declaring use of symbols
+
+document.write("This is the first part" + " and " + "this is the third part "); //Example of concatenation
+
+var B = "Concatenated" + " String"; //Concatination within a variable declaration
+document.write(B); //Writing previously stated variable to file
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro"; //Declaring multiple variables in one line
+document.write(Dad); //Writing one variable to the file
+
+document.write(3+3); //An example of an expression within 
